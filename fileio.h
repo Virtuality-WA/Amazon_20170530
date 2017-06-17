52031d99c3a7793e79e48f0c30310e39d7fe723e
Jose Pagan <DSPEngineer@optonline.net>
2017-06-17 12:09:12 -0700

    Amazon Question, read for log file

---
+++ fileio.h
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   fileio.h
+ * Author: jose
+ *
+ * Created on June 16, 2017, 11:00 PM
+ */
+#pragma once
+
+class fileio
+{
+public:
+    fileio() {};
+    ~fileio() {};
+    
+    
+private:
+    ifstream iFile;
+    
+};
